6f0ad1501804ed6b34bedf0a9de3f1c30b22f7ba
pin-yu-wang <judyww1228@yahoo.com>
2025-10-17 09:25:17 +0000

    gcd

---
+++ gcd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a%b);
+    }
+}
+
+int main()
+{
+printf("GCD:%d\n", gcd(24,40));
+return 0;
+}
